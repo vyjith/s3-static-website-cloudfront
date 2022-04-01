412d64e5355eecde9a83daf29d2a94876922c6a9
Vyjith <38276429+vyjith@users.noreply.github.com>
2022-04-01 05:42:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,23 +29,6 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
 
 }
 
-# # -------------------------------------------------- 
-# # Website configration 
-# # -------------------------------------------------- 
-
-# resource "aws_s3_bucket_website_configuration" "website" {
-#   bucket = aws_s3_bucket.mybucket.id
-
-#   index_document {
-#     suffix = "index.html"
-#   }
-
-#   error_document {
-#     key = "error.html"
-#   }
-
-# }
-
 # -------------------------------------------------- 
 # Website uploading files
 # -------------------------------------------------- 
