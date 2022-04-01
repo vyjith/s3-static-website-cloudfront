88022f7c47eeb09bbdee3dc3da12656e60e3d0ae
Vyjith <38276429+vyjith@users.noreply.github.com>
2022-04-01 05:59:36 +0530

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -1,23 +1,9 @@
-data "template_file" "bucketpolicy"{
-
-    template = "${file("bucketpolicy.json")}"
-}
-
-data "template_file" "bucketpolicyuser"{
-
-    template = "${file("deployer.json")}"
-}
 
 data "aws_route53_zone" "selected" {
   name         = "vyjithks.tk."
   private_zone = false
 }
 
-# output "zone" {
-
-# value = data.aws_route53_zone.selected.id
-
-# }
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
